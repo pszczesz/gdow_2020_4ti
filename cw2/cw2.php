4f914dffe240f27932bbfb176185cb85748fa809
pszczesz <pszczesz@gmail.com>
2020-09-08 08:34:14 +0100

    dd

---
+++ cw2/cw2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .ramka{
+            border-radius: 10px;
+            box-shadow: 5px 5px gray;
+            border: solid black 1px;
+            text-align: center;
+            padding: 15px;
+            margin: 0 20%;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    echo "<div class='ramka'>";
+    echo "<h1>Adam Nowak</h1>\n";
+    echo "<h2>Klasa 4T</h2>\n";
+    echo "<h3>Rok szkolny 2020/21</h3>\n";
+echo "</div>"
+    ?>
+</body>
+
+</html>
