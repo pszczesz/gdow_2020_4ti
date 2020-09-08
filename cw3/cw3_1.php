c09e098303d1fdd6d20544082b6839e6b4a49dcb
pszczesz <pszczesz@gmail.com>
2020-09-08 12:13:56 +0100

    ff

---
+++ cw3/cw3_1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $wybor = false;
+        if($wybor){
+            echo "<p style='color:green;'>Wybrano prawdę!!</p>";
+        }else{
+            echo "<p style='color:red;'>Wybrano fałsz!!</p>";
+        }
+        $wiek = 18;
+        if($wiek<18){
+            echo "<p>Osoba niepełnoletnia</p>";
+        }elseif($wiek==18){
+            echo "<p>Osoba ma 18 lat</p>";
+        }else{
+            echo "<p>Osoba pełnoletnia</p>";
+        }
+    ?>
+</body>
+</html>
