041e0c2b4db0d25ba24e181dbb7ddfb000ba04ae
pszczesz <pszczesz@gmail.com>
2020-10-09 09:05:30 +0200

    ff

--- cw12/wynik.php
+++ cw12/wynik.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+    const passw = "qaz";
+    const log = "admin";
+    if(isset($_POST['login'])){
+        $login = trim($_POST['login']);
+        $password = trim($_POST['haslo']);
+        if(passw==$password && log==$login){
+            echo "<h3>Zalogowano do systemu</h3>\n";
+        }else{
+            echo "<p style='color=red'>Błędny logi lub hasło</p>";
+            echo "<div><a href='cw12.php'>Powrót do formularza</a></div>";
+        }
+    }else{
+        echo "<div><a href='cw12.php'>Powrót do formularza</a></div>";
+    }
+    ?>
 </body>
 </html>
