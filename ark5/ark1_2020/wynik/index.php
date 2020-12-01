bf6ab78a64f09047fc5424e8e46958221bd2e7d8
pszczesz <pszczesz@gmail.com>
2020-12-01 11:40:08 +0100

    dd

--- ark5/ark1_2020/wynik/index.php
+++ ark5/ark1_2020/wynik/index.php
@@ -7,6 +7,58 @@
     <title>Nasz sklep komputerowy</title>
 </head>
 <body>
-    
+   <div id="gora">
+       <div id="menu">
+           <a href="index.php">Główna</a>
+           <a href="procesory.html">Procesory</a>
+           <a href="ram.html">RAM</a>
+           <a href="grafika.html">Grafika</a>
+       </div>
+       <div id="logo">
+           <h2>Podzespoły komputerowe</h2>
+       </div>
+   </div> 
+   <div id="glowny">
+       <h1>Dzisiejsze promocje</h1>
+       <table>
+           <tr>
+               <th>NUMER</th>
+               <th>NAZWA PODZESPOŁU</th>
+               <th>OPIS</th>
+               <th>CENA</th>
+           </tr>
+           <?php
+            $conn = new mysqli("localhost","root",null,"gdow_sklep_2020");
+            if($conn->connect_errno!=0) die("ERROR");
+            $conn->query("SET NAMES utf8");
+            $sql = "SELECT id,nazwa,opis,cena FROM podzespoly WHERE cena<1000";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_row()){
+                echo "<tr>\n";
+                echo "<td>{$row[0]}</td>";
+                echo "<td>{$row[1]}</td>";
+                echo "<td>{$row[2]}</td>";
+                echo "<td class='rightm'>{$row[3]}</td>";
+                echo "</tr>\n";
+            }
+            $conn->close();
+           ?>
+       </table>
+   </div>
+   <div id="stopka">
+       <div>
+           <img src="scalak.jpg" alt="promocja na procesory">
+       </div>
+       <div>
+           <h4>Nasz Sklep Komputerowy</h4>
+           <p>Współpracujemy z hurtownią <a href="http://www.edata.pl">edata</a></p>
+       </div>
+       <div>
+           <p>zadzwoń: 601 602 603</p>
+       </div>
+       <div>
+           <p>Stronę wykonał: 34223455654</p>
+       </div>
+   </div>
 </body>
 </html>
